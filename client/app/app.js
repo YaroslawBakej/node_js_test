0a99cafb26fff7c0f07a2b1c813e1858240a1a29
Yaroslaw Bakej <jarobak1995@gmail.com>
2023-01-14 14:27:34 +0300

    await/then

--- client/app/app.js
+++ client/app/app.js
@@ -1,27 +1,56 @@
 let data = document.querySelector(".getData")
 let arr = []
 
-data.addEventListener("click", async () => {
-    arr=[]
+// data.addEventListener("click", async () => {
+//     arr = []
+//     let url = 'https://fe.it-academy.by/Examples/words_tree/root.txt'
+//     await getData(url)
+
+// })
+
+// async function getData(url) {
+//     let x
+//     try {
+//         let response = await fetch(url)
+//         let data = await response.text()
+//         x = data
+//         let jsonData = JSON.parse(data)
+//         for (let i = 0; i < jsonData.length; i++) {
+//             getData(`https://fe.it-academy.by/Examples/words_tree/${jsonData[i]} `)
+//         }
+//     } catch (error) {
+//         if (typeof x == "string") arr.push(x)
+//     }
+//     let result = document.querySelector('.result')
+//     result.textContent = arr.join(` `)
+// }
+
+data.addEventListener("click", () => {
+    arr = []
     let url = 'https://fe.it-academy.by/Examples/words_tree/root.txt'
-    await getData(url)
-    
+    getData(url)
 })
 
-async function getData(url) {
+function getData(url) {
     let x
-    try {
-        let response = await fetch(url)
-        let data = await response.text()
-        x = data
-        let jsonData = JSON.parse(data)
-        for (let i = 0; i < jsonData.length; i++) {
-            getData(`https://fe.it-academy.by/Examples/words_tree/${jsonData[i]} `)
-        }
-    } catch (error) {
-        if (typeof x == "string") arr.push(x)
-    }
-    
-    let result = document.querySelector('.result')
-    result.textContent = arr.join(` `)
+    fetch(url)
+        .then(response => {
+            let data = response.text()
+            return data
+        })
+        .then(data => {
+            try {
+                x = data
+                let jsonData = JSON.parse(data)
+                for (let i = 0; i < jsonData.length; i++) {
+                    getData(`https://fe.it-academy.by/Examples/words_tree/${jsonData[i]} `)
+                }
+            } catch (error) {
+                if (typeof x == "string") arr.push(x)
+            }
+            let result = document.querySelector('.result')
+            result.textContent = arr.join(` `)
+        })
+
 }
+
